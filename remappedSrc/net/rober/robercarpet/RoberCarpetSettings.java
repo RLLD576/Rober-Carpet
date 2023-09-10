414c80d6cf5c0fc8c4a6e4d2053dfb42f4e9b826
Roberto Lloréns <robllodue@gmail.com>
2023-09-10 18:54:17 +0200

    1.18 initial commit

---
+++ remappedSrc/net/rober/robercarpet/RoberCarpetSettings.java
@@ -0,0 +1,14 @@
+package net.rober.robercarpet;
+
+import carpet.settings.Rule;
+
+public class RoberCarpetSettings {
+    @Rule(desc="Lets dispensers convert dirt into clay with water bottles", category = {"dispenser", "rober"})
+    public static boolean FarmableClay = false;
+
+    @Rule(desc="Sets the game-ticks of delay when you sleep in a bed (Vanilla is 100)",category = "rober")
+    public static int SleepingDelay = 100;
+
+    @Rule(desc="The amount of ticks before thunderstorm that are needed for the mod to warn you about it",category = "rober")
+    public static int ThunderWarn = 0;
+}
