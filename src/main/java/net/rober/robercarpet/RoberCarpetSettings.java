ead7bc60b882c9739e1689483cd42eb83dc00066
Roberto Lloréns <robllodue@gmail.com>
2023-09-12 09:59:35 +0200

    Deleted farmable clay rule

--- src/main/java/net/rober/robercarpet/RoberCarpetSettings.java
+++ src/main/java/net/rober/robercarpet/RoberCarpetSettings.java
@@ -3,8 +3,6 @@ package net.rober.robercarpet;
 import carpet.settings.Rule;
 
 public class RoberCarpetSettings {
-    @Rule(desc="Lets dispensers convert dirt into clay with water bottles. Does nothing in 1.19+", category = {"dispenser", "rober"})
-    public static boolean FarmableClay = false;
 
     @Rule(desc="Sets the game-ticks of delay when you sleep in a bed (Vanilla is 100)",category = "rober")
     public static int SleepingDelay = 100;
