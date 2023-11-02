18466ca6539192f00385622265d65020fd3635ce
Rob3r8 <robllodue@gmail.com>
2023-11-02 14:43:16 +0100

    Added falling block rules

--- src/main/java/net/rober/robercarpet/RoberCarpetSettings.java
+++ src/main/java/net/rober/robercarpet/RoberCarpetSettings.java
@@ -11,4 +11,12 @@ public class RoberCarpetSettings {
 
     @Rule(desc="The amount of ticks before thunderstorm that are needed for the mod to warn you about it",category = "rober")
     public static int ThunderWarn = 0;
+    @Rule(desc="Reintroduce the 1.12 falling block behavior",category={"rober","falling-block"})
+    public static boolean OldFallingBehavior = false;
+
+    @Rule(desc="Age in gameticks at which falling blocks die, -1 for infinity. (Vanilla is 600)",category = {"rober","falling-block"})
+    public static int FallingBlockDieAge = 600;
+
+    @Rule(desc="Falling blocks over walls would not have friction with the floor as in 1.12",category = {"rober","falling-block"})
+    public static boolean FallingBlockNoFrictionWithWalls = false;
 }
